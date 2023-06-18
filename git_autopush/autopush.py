5a218b385513cf95da5e698485e47785f48cfac8
Walter <w.angoye@alustudent.com>
2023-06-18 16:20:04 +0300

    Updated autopush.py

--- git_autopush/autopush.py
+++ git_autopush/autopush.py
@@ -35,20 +35,27 @@ def monitor_directory(path="."):
                 if files[filename] != current_files[filename]
             }
 
+            changes_detected = False  # Flag to track if changes were detected
+
             for file in added_files:
                 commit_message = f"Created {os.path.basename(file)}"
                 add_and_push(file, commit_message)
+                changes_detected = True
 
             for file in deleted_files:
                 commit_message = f"Deleted {os.path.basename(file)}"
                 add_and_push(file, commit_message)
+                changes_detected = True
 
             for file in modified_files:
                 commit_message = f"Updated {os.path.basename(file)}"
                 add_and_push(file, commit_message)
+                changes_detected = True
 
             files = current_files
-            time.sleep(1) # Sleep for 1 second before checking again
+
+            if not changes_detected:
+                time.sleep(1)  # Sleep for 1 second before checking again
 
     except KeyboardInterrupt:
         exit_gracefully(None, None)
