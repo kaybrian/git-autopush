226ee6ce8d2bf4d5dfb95716ea5b16fea4900a39
Walter <w.angoye@alustudent.com>
2023-06-18 15:33:18 +0300

    Updated autopush.py

--- git_autopush/autopush.py
+++ git_autopush/autopush.py
@@ -1,6 +1,8 @@
 import os
 import time
 import subprocess
+import signal
+import sys
 
 def monitor_directory(path="."):
     if not os.path.exists(os.path.join(path, ".git")):
@@ -14,30 +16,40 @@ def monitor_directory(path="."):
             full_path = os.path.join(root, filename)
             files[full_path] = os.stat(full_path).st_mtime
 
-    while True:
-        current_files = {filename: os.stat(filename).st_mtime for filename in files.keys()}
+    def exit_gracefully(signal, frame):
+        print("\nGoodbye!")
+        sys.exit(0)
 
-        added_files = current_files.keys() - files.keys()
-        deleted_files = files.keys() - current_files.keys()
-        modified_files = {
-            filename for filename in files.keys() & current_files.keys()
-            if files[filename] != current_files[filename]
-        }
+    signal.signal(signal.SIGINT, exit_gracefully)
 
-        for file in added_files:
-            commit_message = f"Created {os.path.basename(file)}"
-            add_and_push(file, commit_message)
+    try:
+        while True:
+            current_files = {filename: os.stat(filename).st_mtime for filename in files.keys()}
 
-        for file in deleted_files:
-            commit_message = f"Deleted {os.path.basename(file)}"
-            add_and_push(file, commit_message)
+            added_files = current_files.keys() - files.keys()
+            deleted_files = files.keys() - current_files.keys()
+            modified_files = {
+                filename for filename in files.keys() & current_files.keys()
+                if files[filename] != current_files[filename]
+            }
 
-        for file in modified_files:
-            commit_message = f"Updated {os.path.basename(file)}"
-            add_and_push(file, commit_message)
+            for file in added_files:
+                commit_message = f"Created {os.path.basename(file)}"
+                add_and_push(file, commit_message)
 
-        files = current_files
-        time.sleep(1)  # Sleep for 1 second before checking again
+            for file in deleted_files:
+                commit_message = f"Deleted {os.path.basename(file)}"
+                add_and_push(file, commit_message)
+
+            for file in modified_files:
+                commit_message = f"Updated {os.path.basename(file)}"
+                add_and_push(file, commit_message)
+
+            files = current_files
+            time.sleep(1)  # Sleep for 1 second before checking again
+
+    except KeyboardInterrupt:
+        exit_gracefully(None, None)
 
 def add_and_push(file, commit_message):
     subprocess.run(["git", "add", file])
