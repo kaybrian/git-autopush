e28ee47098cce966cd49dd19e17334202a1e5c5b
Walter <w.angoye@alustudent.com>
2023-06-18 13:20:31 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="git-autopush",
-    version="0.0.1",
+    version="0.0.2",
     author="Chyna",
     author_email="angoyewally@gmail.com",
     description="Automates git functions: git add, git commit, and git push",
