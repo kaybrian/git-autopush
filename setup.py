75f724e006bd400c540424abf52212e505416ed3
Walter <w.angoye@alustudent.com>
2023-06-18 13:01:57 +0300

    Updated setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="git-autopush",
+    version="0.0.1",
+    author="Chyna",
+    author_email="angoyewally@gmail.com",
+    description="Automates git functions: git add, git commit, and git push",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chyna-gvng/git-autopush",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        # List your dependencies here, if any
+    ],
+)
