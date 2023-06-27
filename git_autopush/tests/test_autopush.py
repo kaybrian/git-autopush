85d138ed4ad12761fb22d2c8ad707f4cb288560c
kaybrian <kayonbrian@gmail.com>
2023-06-27 12:02:27 +0200

    Updated test_autopush.py

--- git_autopush/tests/test_autopush.py
+++ git_autopush/tests/test_autopush.py
@@ -8,5 +8,5 @@ from io import StringIO
 class TestDirectoryMonitor(unittest.TestCase):
     def setUp(self):
         self.test_dir = tempfile.mkdtemp()
-        self.path = os.path.join(self.test_dir, "test")
+        self.path = os.path.join(self.test_dir, ".git")
 
